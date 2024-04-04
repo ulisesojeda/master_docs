98173b91e6429ca7424047238a7a8c365f67c338
Ulises <ulises.odysseus22@gmail.com>
2024-04-04 16:33:13 +0200

    update

---
+++ dags/dependencies.py
@@ -0,0 +1,34 @@
+
+import datetime
+from airflow.decorators import dag
+from airflow.operators.bash_operator import BashOperator
+from airflow.models.baseoperator import chain
+
+
+@dag(dag_id="dag_dependencies", start_date=datetime.datetime(2024, 4, 4), schedule="@daily", catchup=False)
+def generate_dag():
+    task_cat = BashOperator(
+        task_id='cat',
+        bash_command='cat /etc/passwd',
+    )
+    
+    task_uname = BashOperator(
+        task_id='uname',
+        bash_command='uname',
+    )
+    
+    task_echo = BashOperator(
+        task_id='echo',
+        bash_command='echo $ENV',
+    )
+
+    task_ls = BashOperator(
+        task_id='ls',
+        bash_command='ls',
+    )
+
+    task_cat, task_uname >> task_echo >> task_ls
+
+    #chain(task_cat, task_uname, task_echo, task_ls)
+
+generate_dag()
